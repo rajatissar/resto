d76bdd72b19605e0580d4109b9c80c6d9ff8576a
Rajat Kumar <rajat.kumar@daffodilsw.com>
2020-06-18 15:41:09 +0530

    components and router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add',
+    component: AddRestoComponent
+  },
+  {
+    path: 'list',
+    component: ListRestoComponent
+  },
+  {
+    path: 'update',
+    component: UpdateRestoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
