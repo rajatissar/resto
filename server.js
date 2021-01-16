f76535decb1fd5f6b2f8a4213cedfe14e54e64e0
Rajat Kumar <rajatissar94@gmail.com>
2021-01-16 21:56:55 +0530

    build issue fixed

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/resto'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/resto/' });
+    res.sendFile(path.join(__dirname, 'dist/resto/index.html'));
 });
 
 // Start the app by listening on the default
